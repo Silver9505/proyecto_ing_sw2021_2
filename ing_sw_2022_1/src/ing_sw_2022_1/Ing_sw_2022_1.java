80fe9f8ff16f5d970fb88fdb7a5108c6da265e4b
Silver9505 <64332710+Silver9505@users.noreply.github.com>
2022-06-07 17:47:01 -0500

    hola

---
+++ ing_sw_2022_1/src/ing_sw_2022_1/Ing_sw_2022_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ing_sw_2022_1;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Ing_sw_2022_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
