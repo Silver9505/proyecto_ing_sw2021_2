8d428e5d02dd65b3e3dc8e7c8939b4ae1e528f6e
Silver9505 <64332710+Silver9505@users.noreply.github.com>
2022-06-08 20:09:29 -0500

    version 3.33

--- ing_sw_2022_1/src/ing_sw_2022_1/Ing_sw_2022_1.java
+++ ing_sw_2022_1/src/ing_sw_2022_1/Ing_sw_2022_1.java
@@ -16,7 +16,7 @@ public class Ing_sw_2022_1 {
     public static void main(String[] args) {
         // TODO code application logic here
         // hola a todos
-        System.out.print ("hola mundo");
+        System.out.print ("hola mundo1");
     }
     
 }
